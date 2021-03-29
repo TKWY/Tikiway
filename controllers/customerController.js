50b9fc9734236e8edf5e8b1fc4221eeefd19b633
tamatini <tamatini@hotmail.fr>
2021-03-28 20:33:21 -1000

    branch + create customer basic method

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,3 +1,4 @@
+const Customer = require('../models/customerModel')
 // handle customers operation
 // get user
 // signin
@@ -7,3 +8,17 @@
 // get orders
 // get past orders
 // get futur orders
+
+const create_customer = (req, res) => {
+  const customer = new Customer(req.body)
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    })
+};
+
+module.exports = {
+  create_customer
+}
