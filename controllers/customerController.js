5590489f71f23d35acbe154b91b32ed578eda93b
tamatini <tamatini@hotmail.fr>
2021-03-28 21:22:13 -1000

    customer create and login method

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -10,15 +10,34 @@ const Customer = require('../models/customerModel')
 // get futur orders
 
 const create_customer = (req, res) => {
-  const customer = new Customer(req.body)
-    .then((result) => {
-      res.send(result);
-    })
-    .catch((err) => {
-      console.log(err);
-    })
+  if (!Customer.find(req.body.phone)) {
+    const newCustomer = new Customer(req.body)
+    newCustomer.save()
+      .then((result) => {
+        res.send(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      }) 
+  } else {
+    res.status(409).send('User Already Exits! Login or enter another phone number');
+  };
+};
+
+const customer_signup = (req, res) => {
+  if (!req.body.phone || !req.body.password) {
+    res.status(401).send('Please enter your phone number and password');
+  } else {
+    Customer.find((customer) => {
+      if (customer.phone === req.body.phone && customer.password === req.body.password) {
+        req.session.customer = customer;
+      };
+    });
+    res.status(401).send('Invalid credential');
+  };
 };
 
 module.exports = {
-  create_customer
-}
+  create_customer,
+  customer_signup
+};
--- routes/customerRoutes.js
+++ routes/customerRoutes.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 const customerController = require('../controllers/customerController');
 
-router.get('/', (req, res) => {
-  res.send('Customer infos')
-});
+router.post('/signup', customerController.create_customer);
+router.post('/signin', customerController.customer_signup);
 // get
 // post
 // put
