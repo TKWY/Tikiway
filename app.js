49e3e87bac7c3d85ae0c7f166e451f6059a0e3e2
tamatini <tamatini@hotmail.fr>
2021-04-01 00:08:29 -1000

    new customer errors handling in error controller, updated readme

--- app.js
+++ app.js
@@ -3,26 +3,30 @@ const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 
+
 // Local imports
 const customerRoutes = require('./routes/customerRoutes');
-const { port, mongodb_user, mongodb_password, mongodb_cluster, mongodb_document } = require('./config')
+const { port, mongodb_user, mongodb_password, mongodb_cluster, mongodb_document } = require('./config');
+
 
 // Running Application
-const app = express()
-
+const app = express();
 app.listen(port, () => {
   console.log(`Listening on http://localhost:${port}`);
 });
 
+
 // Connect to mongodb
 const dbURI = `mongodb+srv://${mongodb_user}:${mongodb_password}@${mongodb_cluster}/${mongodb_document}?retryWrites=true&w=majority`;
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
-  .catch((err) => console.log(err))
+  .catch((err) => console.log(err));
+
 
 // Application options & middleware
-app.use(express.json())
-app.use(express.urlencoded( {extended: true} ))
-app.use(morgan('dev'))
+app.use(express.json());
+app.use(express.urlencoded( {extended: true} ));
+app.use(morgan('dev'));
+
 
 // routes
 app.use('/customers', customerRoutes);
