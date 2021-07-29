1aec85cb93ec305b9bef71e68cf03c3388a9f3d7
tamatini <tamatini@hotmail.fr>
2021-07-29 11:10:28 -1000

    post new dish method refactor

--- src/db/controllers/dishesController.js
+++ src/db/controllers/dishesController.js
@@ -11,24 +11,26 @@ const Restaurant = require('../models/restaurantModels');
 // Method will return all the dishes of specified menu as a response
 // it will get a list of dishes but yuou have to specify the restaurant Id
 // and the menu Id.
+// route: GET restaurants/:restaurantId/menu/:menuId/dish
 getDishes = async (req, res) => {
   const { restaurantId, menuId } = req.params
   try {
     const findRestaurant = await Restaurant.findById(restaurantId);
     const findMenu = await findRestaurant.menu.id(menuId);
     const findDish = await findMenu.dishes;
-    if (findDish === "") {
-      res.sendStatus(404);
+    if (findDish === null ) {
+      return res.sendStatus(404);
     }
-    res.status(200).json(findDish);
+    return res.status(200).json(findDish);
   } catch (err) {
-    res.json(err); // Need to check errors and return a better status code in case of an error
+    return res.json(err); // Need to check errors and return a better status code in case of an error
   }
 };
 
 // Method will return dish with specified id
 // Same as the get all dishes method, you need to specify the restaurant Id and
 // menu Id.
+// route: GET restaurants/:restaurantId/menu/:menuId/dish/:dishId
 getDishById = async (req, res) => {
   const { restaurantId, menuId, dishId } = req.params;
   try {
@@ -36,48 +38,42 @@ getDishById = async (req, res) => {
     const findMenu = await findRestaurant.menu.id(menuId);
     const findDish = findMenu.dishes.id(dishId);
     if (findDish === null) {
-      res.sendStatus(404);
+      return res.sendStatus(404)
     }
-    res.status(200).json(findDish);
+    return res.status(200).json(findDish);
   } catch (err) {
-    res.json(err); // Need to check errors and return a better stataus code in case of an error
+    return res.json(err); // Need to check errors and return a better stataus code in case of an error
   }
 };
 
-postDish = (req, res) => {
+// Method will post a new dish in specified menu Id
+// Need restaurant Id and menu Id
+// route: POST restaurants/:restaurantId/menu/:menuId/dish/:dishId
+postDish = async(req, res) => {
   const body = req.body;
-  const param = req.params;
-  const newDish = new Dish({
-    name: body.name,
-    description: body.description,
-    price: body.price,
-    promoPrice: body.promoPrice,
-    image: body.image,
-    restaurantId: param.restaurantId,
-    menuId: param.menuId
-  });
-  Restaurant.findById(req.params.restaurantId)
-    .then(restaurant => {
-      const menu = restaurant.menu.id(req.params.menuId);
-      const dishes = menu.dishes
-      dishes.push(newDish)
-      restaurant.save()
-        .then(result => {
-          const menuFilter = result.menu.id(req.params.menuId)
-          const dish = menuFilter.dishes
-          res.status(201).json(dish[dish.length - 1]);
-        })
-        .catch(err => { 
-          if (err) {
-            res.status(500).json(err)
-          }
-        })
-    })
-    .catch(err => { 
-      if (err) {
-        res.status(404).json('Restaurant does not exist');
-      }
-    })
+  const { restaurantId, menuId } = req.params;
+  try {
+    const findRestaurant = await Restaurant.findById(restaurantId);
+    const findMenu = await findRestaurant.menu.id(menuId);
+    const dish = await findMenu.dishes;
+    const newDish = await new Dish({
+      name: body.name,
+      description: body.description,
+      price: body.price,
+      promoPrice: body.promoPrice,
+      image: body.image,
+      restaurantId: restaurantId,
+      menuId: menuId
+    });
+    dish.push(newDish);
+    if (findRestaurant.save()) {
+      const findDish = await dish[dish.length - 1];
+      return res.status(201).json(findDish);
+    }
+    res.sendStatus(500);
+  } catch (err) {
+    return res.json(err); // Need to handle errors
+  }
 };
 
 updateDish = (req, res) =>  {
