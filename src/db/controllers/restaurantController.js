3c2552de88372e02b9d63fcc4b38fe422e703cb0
Tamatini <tamatini@hotmail.fr>
2021-07-30 05:48:50 -1000

    refactor restaurant update&delete methods

--- src/db/controllers/restaurantController.js
+++ src/db/controllers/restaurantController.js
@@ -5,7 +5,7 @@
 const Restaurant = require('../models/restaurantModels');
 
 // Method will return a list of all restaurant
-// route: restaurants
+// route: GET restaurants
 getAllRestaurant = async (req, res) => {
   try {
     const findRestaurants = await Restaurant.find();
@@ -22,7 +22,7 @@ getAllRestaurant = async (req, res) => {
 };
 
 // Method will return restaurant with specified Id
-// route: restaurants/:restaurantId
+// route: GET restaurants/:restaurantId
 getRestaurantById = async (req, res) => {
   const id = req.params.restaurantId;
   try {
@@ -44,6 +44,7 @@ getRestaurantById = async (req, res) => {
 // restaurant will be created by admins only and password generated automatically
 // as and ease of use front end will be a web application only so restaurant owned can access
 // their background from any device
+// route POST restaurants
 postRestaurant = async(req, res) => {
   const body = req.body;
   try {
@@ -54,22 +55,38 @@ postRestaurant = async(req, res) => {
     if (err.code === 11000) {
       return res.status(409).json({error: 'A restaurant with that name already exist'});
     }
+    console.log(err.error);
     return res.status(500).json({error: 'Internal Server Error'})
   }
 };
 
 // Method will update restaurant informations with specified Id
 // restaurant owner can change their information themselve
-updateRestaurant = (req, res) => {
-  Restaurant.findByIdAndUpdate(req.params.restaurantId, req.body)
-    .then(() => res.status(201).json({msg: 'Restaurant has been updated'}))
-    .catch(err => {
-      if (err) {
-        return res.status(404).json(err)
-      }
-    })
+// route PUT restaurants/:restaurantIdd
+updateRestaurant = async (req, res) => {
+  const id = req.params.restaurantId;
+  const { name, description, category, image } = req.body;
+  const updatedRestaurant = {
+    name: name,
+    description: description,
+    image: image,
+    category: category
+  }
+  try {
+    const findRestaurant = await Restaurant.findByIdAndUpdate(id, updatedRestaurant);
+    return res.status(201).json(findRestaurant);
+  } catch (err) {
+    if (err.code === 11000) {
+      return res.status(409).json({error: 'A restaurant with that name already exist'});
+    }
+    console.log(err.error);
+    return res.status(500).json({error: 'Internal Server Error'});
+  }
 };
 
+// Method will delete restaurant with specified Id
+// it is accessible to admins only
+// route: DELETE restaurants/:restaurantId
 deleteRestaurant = (req, res) => {
   Restaurant.findByIdAndDelete(req.params.restaurantId)
     .then(() => res.sendStatus(204))
@@ -80,6 +97,7 @@ deleteRestaurant = (req, res) => {
     })
 };
 
+// Export all methods on restaurantController
 module.exports = {
   getAllRestaurant,
   getRestaurantById,
